bd02831457f8fb1b2ff43871891cd3c0d3fad9e1
Schrodingers-Cats <adityasen120@gmail.com>
2022-06-16 18:34:08 -0400

    add media to tweet, download image, attach image, delete image

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require('dotenv').config(); //start reading .env files
-
 //node modules
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const { TwitterApi } = require('twitter-api-v2');
+const mongoose = require('mongoose');
+const fs = require("fs");
+const axios = require('axios').default;
+
 
 /**************************** NASA API Functions ****************************/
 //get NASA API Key
@@ -10,11 +12,13 @@ const apiKeyNasa = process.env.NASA_API_KEY;
 
 //get NASA APOD data
 async function getAPOD() {
-    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKeyNasa}`) //API URI for APOD NASA API
+    const apodURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKeyNasa}`;
+
+    //make https request
+    const response = await axios.get(apodURL)
     
-    const data = await response.json() //make data readable
-        
-    return data; //send promise of APOD data
+    //send data back 
+    return response.data;
 }
 
 
@@ -38,21 +42,40 @@ Tweet me a Universe --Test 2.0
 
 ${apodImageTitle}`;
 
-    const response = await fetch(apodImageURL);
-    const imageData = await response.blob();
-    const localImgURL = URL.createObjectURL(imageData);
-    
-    // // First, post all your images to Twitter
-    // const mediaIds = await Promise.all([
-    //     // file path
-    //     client.v1.uploadMedia(apodImageURL),
-    //     // from a buffer, for example obtained with an image modifier package
-    //     //client.v1.uploadMedia(Buffer.from(rotatedImage), { type: 'png' }),
-    // ]);
-    // console.log(mediaIds);
+    //get image and save it
+    const localImageURL = "./apodImage.jpg";
+    // GET request for remote image in node.js using Axios
+    const imageData = await axios({
+        method: 'get',
+        url: apodImageURL,
+        responseType: 'stream'
+    })
+    const imageUpload = imageData.data.pipe(fs.createWriteStream(localImageURL)); //make image in local machine
 
-    //await client.v1.tweet(tweetText, { media_ids: mediaIds });
+    //when finish downloading image
+    imageUpload.on("finish", async () => {
+        const imageUploadId = await client.v1.uploadMedia(localImageURL); //attach image to tweet
+
+        //tweet
+        await client.v1.tweet(tweetText, { media_ids: imageUploadId }); //make post request to Twitter to post tweet
+
+        //delete image
+        fs.rm(localImageURL, (err) => {
+            //check for error
+            if (err) {
+                console.log(err)
+            }
+            //no error
+            else {
+                console.log("Deleted File Successfully");
+            }
+        });
+    });
 }
+
+
+/**************************** MongoDB Database ****************************/
+
 
 
 /**************************** RUN SERVER ****************************/
