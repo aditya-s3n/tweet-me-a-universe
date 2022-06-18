769045c4d2d3aaf3cf05000022f9557c252ee611
Schrodingers-Cats <adityasen120@gmail.com>
2022-06-18 15:27:15 -0400

    add heroku procfile + test

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ ${apodImageTitle}`;
                 console.log("Deleted Apod Image Successfully");
 
                 //update day and APOD number for Database
-                updateApodData();
+                //updateApodData();
             }
         });
     });
@@ -146,7 +146,7 @@ async function findAllData() {
 
 /**************************** RUN SERVER + SCHEDULER ****************************/
 const rule = new schedule.RecurrenceRule(); //start the scheduler to recur to the rule
-rule.hour = 7; //recur at 7am everday
+rule.hour = 14; //recur at 7am everday
 rule.minute = 0; //should only recur at 7:00am 
 rule.tz = "EST"; //change timezone to eastern standard time
 
